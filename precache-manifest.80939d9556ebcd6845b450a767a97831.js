febd55ab3ab681d903cc6b30e78834d5392e7973
mohammad-amir <amirbbd89@gmail.com>
2019-11-20 23:39:54 +0000

    Deploying to master from master 6e052bb684196c6e7ad6b25affdacd5bb7c173f1

---
+++ precache-manifest.80939d9556ebcd6845b450a767a97831.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89deca6bc1e023219555c06edaabbf9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebec65fe8d82368da949",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "275a9cd351e9e7e11e3f",
+    "url": "/static/js/2.3d9806eb.chunk.js"
+  },
+  {
+    "revision": "ebec65fe8d82368da949",
+    "url": "/static/js/main.06a502c3.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
