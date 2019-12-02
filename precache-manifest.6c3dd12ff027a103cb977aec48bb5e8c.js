844cc46026acda1b86c0447401a00c61ed5cc901
mohammad-amir <amirbbd89@gmail.com>
2019-12-02 21:46:58 +0000

    Updates

---
+++ precache-manifest.6c3dd12ff027a103cb977aec48bb5e8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30c5e9a53a8ade0446d5d2678b019fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad3ec8f8290bcd127175",
+    "url": "/static/css/main.6138affa.chunk.css"
+  },
+  {
+    "revision": "4347ccf15f8757139bac",
+    "url": "/static/js/2.0096a813.chunk.js"
+  },
+  {
+    "revision": "ad3ec8f8290bcd127175",
+    "url": "/static/js/main.54e7efab.chunk.js"
+  },
+  {
+    "revision": "92f416338fa5b92447d6",
+    "url": "/static/js/runtime-main.79fbf0b5.js"
+  },
+  {
+    "revision": "b8935fb98d1a695260a5ec824fb2beed",
+    "url": "/static/media/home.b8935fb9.jpg"
+  },
+  {
+    "revision": "23060ec23044bcbdbc40a2c6ec428096",
+    "url": "/static/media/preloader.23060ec2.gif"
+  },
+  {
+    "revision": "cd376fcfd6f92a46c089bed821c8b7d8",
+    "url": "/static/media/service-bg.cd376fcf.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.204b6ed1a23d5cb15d28f690d7174a32.js"
+  "/precache-manifest.6c3dd12ff027a103cb977aec48bb5e8c.js"
 );
 
 self.addEventListener('message', (event) => {
